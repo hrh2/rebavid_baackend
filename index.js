67f366946d0329bd8471d9e9d2fca78d9db12e2c
Hirw Hope <gakundohope5@gmail.com>
2024-05-21 21:54:10 +0200

    header and cors policy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const http = require('http');
+const socketio = require('socket.io');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 5000;
@@ -15,9 +16,18 @@ const messageRoute = require('./Controllers/REST/Message');
 
 const app = express();
 const server = http.createServer(app);
+const io = socketio(server, {
+  cors: {
+    origin: '*',
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Authorization", "X-My-Custom-Header"],
+    credentials: true
+  }
+});
 const setupSocket = require('./Controllers/REALTIME/sockets');
 
 app.use(cors());
+
 app.use(express.json({ limit: '1500mb' }));
 app.use(express.urlencoded({ extended: true, limit: '1500mb' }));
 
@@ -38,7 +48,7 @@ app.get('/', async (req, res) => {
 });
 
 // Setup Socket.IO
-setupSocket(server);
+setupSocket(io);
 
 // Start server
 server.listen(PORT, () => {
