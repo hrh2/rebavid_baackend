56b116666176e69c3427e0e4e564ff17ee3bcf9f
Hirw Hope <gakundohope5@gmail.com>
2024-05-22 06:32:43 +0200

    handling message acknowledgment

--- Controllers/REALTIME/SendMessages.js
+++ Controllers/REALTIME/SendMessages.js
@@ -1,8 +1,8 @@
 const { Message, validateMessage } = require('../../Models/Message');
-const {verifyToken} = require('../../Middlewares/Token-verification')
+const { verifyToken } = require('../../Middlewares/Token-verification');
 
 module.exports = (socket, io) => {
-    socket.on('send_message',async (data) => {
+    socket.on('send_message', async (data) => {
         try {
             const { error } = await validateMessage(data);
             if (error) {
@@ -12,9 +12,9 @@ module.exports = (socket, io) => {
             const message = new Message(data);
             await message.save();
             io.emit('new_message', message);  // Broadcast the message to all connected clients
+            socket.emit('message_sent', { message: 'Message sent successfully!' });  // Acknowledge the sender
         } catch (error) {
             socket.emit('error_message', { message: error.message });
         }
     });
 };
-
